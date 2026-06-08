335cf56b6bb297dfecf6ba930852df45489d57ab
Alif Ramdhani <EMAIL_GITHUB_KAMU>
2026-06-08 23:31:17 +0700

    update flask project

--- app.py
+++ app.py
@@ -466,5 +466,8 @@ def sort_ipk():
 # RUN
 # ==================================================
 
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
